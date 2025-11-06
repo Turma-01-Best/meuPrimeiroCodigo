e75c88883d8a71e51fd0cebeb5f939509b86acb1
Turma-01-Best <turmabestcode@gmail.com>
2025-11-05 21:45:19 -0300

    subindo codigo

--- src/main/java/aula02/aula2.java
+++ src/main/java/aula02/aula2.java
@@ -2,21 +2,6 @@ package aula02;
 
 public class aula2 {
 
-	public static void main(String[] args) {
-		calculadora cal = new calculadora();
-
-		int n1 = 10;
-		int n2 = 25;
-
-		// System.out.println("soma: " + somar(n1, n2));
-		// System.out.println("subtraçao:" + (n1 - n2));
-		// System.out.println("multiplicacao:" + (n1 * n2));
-		// System.out.println("Divisao:" + (n1 * n2));
-		System.out.println("soma: " + cal.somar(n1, n2));
-		System.out.println("subtracao: " + cal.subtrair(n1, n2));
-		System.out.println("multiplicao: " + cal.multiplicacao(n1, n2));
-		System.out.println("divisao: " + cal.divisao(n1, n2));
-
-	}
+	
 
 }
